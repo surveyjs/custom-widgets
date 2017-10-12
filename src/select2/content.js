f9d4a10d86138a7c10601e24724f4c479e212a30
Andrew Telnov (DevExpress) <Telnov@corp.devexpress.com>
2017-10-12 11:43:26 +0300

    One code for all platforms

--- src/select2/content.js
+++ src/select2/content.js
@@ -1,74 +1,49 @@
 Survey.JsonObject.metaData.addProperty("dropdown", {name: "renderAs", default: "standard", choices: ["standard", "select2"]});
 
-if (PLATFORM !== "vue") {
-    var widget = {
-        name: "select2",
-        htmlTemplate: "<select style='width: 100%;'></select>",
-        isFit : function(question) { return question["renderAs"] === 'select2'; },
-        afterRender: function(question, el) {
-            if (PLATFORM == "knockout") {
-                var $el = $(el);
-            } else {
-                var $el = $(el).find("select");
-            }
-
-            var widget = $el.select2({
-                theme: "classic"
-            });
-            var updateChoices = function() {
-                $el.select2({data: question.visibleChoices.map(function(choice) { return { id: choice.value, text: choice.text }; })});
-            }
-            question.choicesChangedCallback = updateChoices;
-            updateChoices();
-            $el.on('select2:select', function (e) {
-                question.value = e.target.value;
-            });
-            var updateHandler = function() {
-                $el.val(question.value).trigger("change");
-            };
-            question.valueChangedCallback = updateHandler;
-            updateHandler();
+var widget = {
+    name: "select2",
+    htmlTemplate: "<select style='width: 100%;'></select>",
+    isFit : function(question) { return question["renderAs"] === 'select2'; },
+    afterRender: function(question, el) {
+        var $el = $(el).is("select") ? $(el) : $(el).find("select");
+        var othersEl = document.createElement("input");
+        othersEl.type = "text";
+        othersEl.style.marginTop = "3px";
+        othersEl.style.display = "none";
+        othersEl.style.width = "100%";
+        $el.parent().get(0).append(othersEl);
+        var widget = $el.select2({
+            theme: "classic"
+        });
+        var updateValueHandler = function() {
+            $el.val(question.value).trigger("change");
+            othersEl.style.display = !question.isOtherSelected ? "none": "";
+        };
+        var updateCommentHandler = function() {
+            othersEl.value = question.comment ? question.comment : "";
         }
-    }
-
-    if (PLATFORM !== "knockout") {
-        widget.willUnmount = function(question, el) {
-            $(el).find("select").off('select2:select').select2("destroy");
+        var othersElChanged = function() {
+            question.comment = othersEl.value;
         }
+        var updateChoices = function() {
+            $el.select2({data: question.visibleChoices.map(function(choice) { return { id: choice.value, text: choice.text }; })});
+            updateValueHandler();
+            updateCommentHandler();
+        }
+        question.choicesChangedCallback = updateChoices;
+        updateChoices();
+        $el.on('select2:select', function (e) {
+            question.value = e.target.value;
+        });
+        othersEl.onchange = othersElChanged;
+        question.valueChangedCallback = updateValueHandler;
+        question.commentChangedCallback = updateCommentHandler;
+        updateValueHandler();
+        updateCommentHandler();
     }
 }
-if (PLATFORM === "vue") {
-    var widget = {
-        name: "select2",
-        isFit : function(question) { return question["renderAs"] === 'select2'; }
-    }
-
-    Vue.component(widget.name, {
-        props: ['question', 'css', 'isEditMode'],
-        template: "<select style='width: 100%;'></select>",
-        mounted: function () {
-            var vm = this;
-            $(vm.$el).select2({
-                data: vm.question.choices.map(function(choice) { return { id: choice.value, text: choice.text }; }),
-                theme: "classic"
-            });
-            vm.question.choicesChangedCallback = function() {
-                $(vm.$el).select2({data: vm.question.visibleChoices.map(function(choice) { return { id: choice.value, text: choice.text }; })});
-            }
-            $(vm.$el).on('select2:select', function (e) {
-            vm.question.value = e.target.value;
-            });
-            var updateHandler = function() {
-                $(vm.$el).val(vm.question.value).trigger("change");
-            }
-            vm.question.valueChangedCallback = updateHandler;
-            updateHandler();
-        },
-        destroyed: function () {
-            $(this.$el).off('select2:select').select2("destroy");
-        }
-    });
+widget.willUnmount = function(question, el) {
+    $(el).find("select").off('select2:select').select2("destroy");
 }
 
 Survey.CustomWidgetCollection.Instance.addCustomWidget(widget);
-
