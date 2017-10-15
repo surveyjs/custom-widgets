e421899ad1ddba0c964dfe632447f41094c89273
Andrew Telnov (DevExpress) <Telnov@corp.devexpress.com>
2017-10-15 15:57:31 +0300

    Update image picker widget

--- src/imagepicker.js
+++ src/imagepicker.js
@@ -6,13 +6,14 @@ var widget = {
     isDefaultRender: true,
     activatedByChanged: function(activatedBy) {
         Survey.JsonObject.metaData.addClass("imageitemvalues", [{name: "imageLink"}], null, "itemvalue");
-        Survey.JsonObject.metaData.addClass("imagepicker", ["choices:imageitemvalues", {name:"showLabel:boolean", default: false}], null, "dropdown");
+        Survey.JsonObject.metaData.addClass("imagepicker", [{name: "choices:imageitemvalues", onGetValue: function (obj) { return Survey.ItemValue.getData(obj.choices); }, onSetValue: function (obj, value) { obj.choices = value; }}, 
+            {name:"showLabel:boolean", default: false}, {name:"hasOther", visible: false}], null, "dropdown");
     },
     afterRender: function(question, el) {
         var $el = $(el).is("select") ? $(el) : $(el).find("select");
         var options = $el.find('option');
         for (var i=1; i<options.length; i++) {
-            $(options[i]).data("imgSrc", options[i].text);
+            $(options[i]).data("imgSrc", options[i].imageLink);
             options[i].selected = question.value == options[i].value;
         }
         $el.imagepicker({
