6ed485485b9386cd1b27c56d7b12ac324b3d457e
tsv2013 <tsv2013@users.noreply.github.com>
2018-05-29 18:02:08 +0300

    Fixed https://surveyjs.answerdesk.io/ticket/details/T643 - iCheck processed selection wrong

--- src/icheck.js
+++ src/icheck.js
@@ -3,34 +3,36 @@ function init(Survey, $) {
   var widget = {
     className: "iradio_square-blue",
     name: "icheck",
-    widgetIsLoaded: function() {
+    widgetIsLoaded: function () {
       return !!$.fn.iCheck;
     },
-    isFit: function(question) {
+    isFit: function (question) {
       var t = question.getType();
       return t === "radiogroup" || t === "checkbox" || t === "matrix";
     },
     isDefaultRender: true,
-    afterRender: function(question, el) {
+    afterRender: function (question, el) {
       var rootWidget = this;
       var $el = $(el);
-      $el.find("input").data({ iCheck: undefined });
+      $el.find("input").data({
+        iCheck: undefined
+      });
 
       $el.find("input").iCheck({
         checkboxClass: rootWidget.className,
         radioClass: rootWidget.className
       });
-      var select = function() {
+      var select = function () {
         if (question.getType() !== "matrix") {
           var values = question.value;
           if (!Array.isArray(values)) {
             values = [values];
           }
-          values.forEach(function(value) {
+          values.forEach(function (value) {
             $el.find("input[value=" + value + "]").iCheck("check");
           });
         } else {
-          question.generatedVisibleRows.forEach(function(row, index, rows) {
+          question.generatedVisibleRows.forEach(function (row, index, rows) {
             if (row.value) {
               $(el)
                 .find(
@@ -41,33 +43,37 @@ function init(Survey, $) {
           });
         }
       };
-      $el.find("input").on("ifChecked", function(event) {
+      $el.find("input").on("ifChecked", function (event) {
         if (question.getType() === "matrix") {
-          question.generatedVisibleRows.forEach(function(row, index, rows) {
+          question.generatedVisibleRows.forEach(function (row, index, rows) {
             if (row.fullName === event.target.name) {
               row.value = event.target.value;
             }
           });
         } else if (question.getType() === "checkbox") {
           var oldValue = question.value || [];
-          question.value = oldValue.concat([event.target.value]);
+          var index = oldValue.indexOf(event.target.value);
+          if (index === -1) {
+            question.value = oldValue.concat([event.target.value]);
+          }
         } else {
           question.value = event.target.value;
         }
       });
-      $el.find("input").on("ifUnchecked", function(event) {
+      $el.find("input").on("ifUnchecked", function (event) {
         if (question.getType() === "checkbox") {
           var oldValue = question.value || [];
           var index = oldValue.indexOf(event.target.value);
           if (index >= 0) {
-            question.value = oldValue.splice(index, 1);
+            oldValue.splice(index, 1);
+            question.value = oldValue;
           }
         }
       });
       question.valueChangedCallback = select;
       select();
     },
-    willUnmount: function(question, el) {
+    willUnmount: function (question, el) {
       var $el = $(el);
       $el.find("input").iCheck("destroy");
     }
