109773958383f82601c1bc1a15ff9b91930b1611
dmitrykurmanov <kurmanov.work@gmail.com>
2018-06-09 11:00:07 +0300

    work for https://surveyjs.answerdesk.io/ticket/details/T645 (added select2 config to tagbox)

--- src/select2-tagbox.js
+++ src/select2-tagbox.js
@@ -4,63 +4,87 @@ function init(Survey, $) {
     name: "tagbox",
     title: "Tag box",
     iconName: "icon-tagbox",
-    widgetIsLoaded: function () {
+    widgetIsLoaded: function() {
       return typeof $ == "function" && !!$.fn.select2;
     },
     defaultJSON: {
       choices: ["Item 1", "Item 2", "Item 3"]
     },
     htmlTemplate: "<select multiple='multiple' style='width: 100%;'></select>",
-    isFit: function (question) {
+    isFit: function(question) {
       return question.getType() === "tagbox";
     },
-    activatedByChanged: function (activatedBy) {
+    activatedByChanged: function(activatedBy) {
       Survey.JsonObject.metaData.addClass(
-        "tagbox", [{
-          name: "hasOther",
-          visible: false
-        }],
+        "tagbox",
+        [
+          {
+            name: "hasOther",
+            visible: false
+          }
+        ],
         null,
         "checkbox"
       );
+      Survey.JsonObject.metaData.addProperty("tagbox", {
+        name: "select2Config",
+        default: null
+      });
     },
-    afterRender: function (question, el) {
+    afterRender: function(question, el) {
+      var settings = question.select2Config;
       var $el = $(el).is("select") ? $(el) : $(el).find("select");
       $el.select2({
         tags: "true",
         disabled: question.isReadOnly,
         theme: "classic"
       });
-      var updateValueHandler = function () {
+      var updateValueHandler = function() {
         $el.val(question.value).trigger("change");
       };
-      var updateChoices = function () {
+      var updateChoices = function() {
         $el.select2().empty();
-        $el.select2({
-          data: question.visibleChoices.map(function (choice) {
-            return {
-              id: choice.value,
-              text: choice.text
-            };
-          })
-        });
+
+        if (settings) {
+          if (settings.ajax) {
+            $el.select2(settings);
+          } else {
+            settings.data = question.visibleChoices.map(function(choice) {
+              return {
+                id: choice.value,
+                text: choice.text
+              };
+            });
+            $el.select2(settings);
+          }
+        } else {
+          $el.select2({
+            data: question.visibleChoices.map(function(choice) {
+              return {
+                id: choice.value,
+                text: choice.text
+              };
+            })
+          });
+        }
+
         updateValueHandler();
       };
 
-      var readOnlyUpdater = function (sender, options) {
+      var readOnlyUpdater = function(sender, options) {
         if (options.name === "isReadOnly") {
           $el.prop("disabled", question.isReadOnly);
         }
-      }
+      };
       question._readOnlyUpdater = readOnlyUpdater;
       question.onPropertyChanged.add(readOnlyUpdater);
 
       question.choicesChangedCallback = updateChoices;
       question.valueChangedCallback = updateValueHandler;
-      $el.on("select2:select", function (e) {
+      $el.on("select2:select", function(e) {
         question.value = (question.value || []).concat(e.params.data.id);
       });
-      $el.on("select2:unselect", function (e) {
+      $el.on("select2:unselect", function(e) {
         var index = (question.value || []).indexOf(e.params.data.id);
         if (index !== -1) {
           var val = question.value;
@@ -70,7 +94,7 @@ function init(Survey, $) {
       });
       updateChoices();
     },
-    willUnmount: function (question, el) {
+    willUnmount: function(question, el) {
       $(el)
         .find("select")
         .off("select2:select")
