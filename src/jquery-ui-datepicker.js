019961848d7e978ea48140625e76062996a955e2
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-07-29 17:53:06 +0300

    Fix #132

--- src/jquery-ui-datepicker.js
+++ src/jquery-ui-datepicker.js
@@ -1,17 +1,20 @@
 function init(Survey, $) {
   $ = $ || window.$;
+  if (!!$.fn.datepicker && !!$.fn.datepicker.noConflict) {
+    $.fn.bootstrapDP = $.fn.datepicker.noConflict();
+  }
   var widget = {
     name: "datepicker",
     title: "Date picker",
     iconName: "icon-datepicker",
-    widgetIsLoaded: function() {
+    widgetIsLoaded: function () {
       return !!$ && !!$.fn.datepicker;
     },
-    isFit: function(question) {
+    isFit: function (question) {
       return question.getType() === "datepicker";
     },
     htmlTemplate: "<input class='form-control widget-datepicker' type='text'>",
-    activatedByChanged: function(activatedBy) {
+    activatedByChanged: function (activatedBy) {
       Survey.JsonObject.metaData.addClass(
         "datepicker",
         [
@@ -30,7 +33,7 @@ function init(Survey, $) {
         default: null
       });
     },
-    afterRender: function(question, el) {
+    afterRender: function (question, el) {
       var $el = $(el).is(".widget-datepicker")
         ? $(el)
         : $(el).find(".widget-datepicker");
@@ -51,7 +54,7 @@ function init(Survey, $) {
         };
       }
       if (config.onSelect === undefined) {
-        config.onSelect = function(dateText) {
+        config.onSelect = function (dateText) {
           isSelecting = true;
           question.value = dateText;
           isSelecting = false;
@@ -59,10 +62,10 @@ function init(Survey, $) {
         };
       }
       config.fixFocusIE = false;
-      config.onClose = function(dateText, inst) {
+      config.onClose = function (dateText, inst) {
         this.fixFocusIE = true;
       };
-      config.beforeShow = function(input, inst) {
+      config.beforeShow = function (input, inst) {
         var result = !!navigator.userAgent.match(/Trident\/7\./)
           ? !this.fixFocusIE
           : true;
@@ -71,18 +74,18 @@ function init(Survey, $) {
       };
       var pickerWidget = $el.datepicker(config);
 
-      $el.keyup(function(e) {
+      $el.keyup(function (e) {
         if (e.keyCode == 8 || e.keyCode == 46) {
           $.datepicker._clearDate(this);
         }
       });
 
-      question.readOnlyChangedCallback = function() {
+      question.readOnlyChangedCallback = function () {
         $el.datepicker("option", "disabled", question.isReadOnly);
       };
       question.readOnlyChangedCallback();
 
-      question.valueChangedCallback = function() {
+      question.valueChangedCallback = function () {
         if (!isSelecting) {
           if (question.value) {
             pickerWidget.datepicker("setDate", question.value);
@@ -94,7 +97,7 @@ function init(Survey, $) {
       };
       question.valueChangedCallback();
     },
-    willUnmount: function(question, el) {
+    willUnmount: function (question, el) {
       var $el = $(el).is(".widget-datepicker")
         ? $(el)
         : $(el).find(".widget-datepicker");
